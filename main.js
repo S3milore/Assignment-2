b1d57278270489c1f83ca3884d0c2c2b44ef716f
Your Name <semiloreawolusi@gmail.com>
2022-10-14 09:50:23 -0400

    Second function

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-//A counter that counts up to 1000, allows user specify the
-//count step and generates random numbers without 'math.random()'
 //let arr = [];
 
 function counter(val, step) {
     let arr = [val];
-      for (let i = 0; i < 1000; i += step)
-        arr.push(val += step);
-      for (j = 0; j <= 1000; j++) {
-        if (arr[j] >= 1000) {
-          do {
-            arr.pop();
-          } while (arr[j] > 1000)
-          return arr;
-        }
+    for (let i = 0; i < 1000; i += step)
+      arr.push(val += step);
+    for (j = 0; j <= 1000; j++) {
+      if (arr[j] >= 1000) {
+        do {
+          arr.pop();
+        } while (arr[j] > 1000)
+        return arr;
       }
     }
-    console.log(counter(10, 100));
-    
+  }
+  function numGen(max, min) {
+      const result = Math.floor(Math.random() * (max - min + 1)) + min;
+      return result;
+    };
+  console.log(counter(10, 100));
+  console.log(numGen(20, 2))
+  
