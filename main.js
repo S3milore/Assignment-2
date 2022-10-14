8e282a5041025d7483a5cc6b0e8834e4ccbe928f
Your Name <semiloreawolusi@gmail.com>
2022-10-14 09:34:46 -0400

    fix:

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 //A counter that counts up to 1000, allows user specify the
 //count step and generates random numbers without 'math.random()'
-let arr=[];
-function counter(val,step){
-for(let i=0; i<1000; i++)
-arr.push(val+=step);
-return arr;
-}
-console.log(counter(1,1));
+//let arr = [];
+
+function counter(val, step) {
+    let arr = [val];
+      for (let i = 0; i < 1000; i += step)
+        arr.push(val += step);
+      for (j = 0; j <= 1000; j++) {
+        if (arr[j] >= 1000) {
+          do {
+            arr.pop();
+          } while (arr[j] > 1000)
+          return arr;
+        }
+      }
+    }
+    console.log(counter(10, 100));
+    
